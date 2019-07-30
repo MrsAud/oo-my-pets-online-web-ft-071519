c2a500ca7d4379b8026c4c3c7218a237ace5a2ea
Audrea Cook <audreacwork@gmail.com>
2019-07-30 20:42:22 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,6 +1,6 @@
 class Cat
   
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   attr_reader :name
   
   def initialize(name, owner)
@@ -10,6 +10,7 @@ class Cat
     else
       @owner = Owner.new(owner)
     end
+    @mood = "nervous"
     # binding.pry
   end
   
