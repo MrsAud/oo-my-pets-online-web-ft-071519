7f40d93c22ca4b1486126a576a5cc5ca21049b74
Audrea Cook <audreacwork@gmail.com>
2019-07-30 20:34:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,20 @@
 class Owner
   attr_reader :name, :species
+  @@all = []
   
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
   end
   
   def say_species
     "I am a #{self.species}."
   end
+  
+  def self.all
+    @@all
+  end
+  
+  
 end
