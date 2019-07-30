2723f1f3cae37f96f86f77764ad143e1fb1e355b
Audrea Cook <audreacwork@gmail.com>
2019-07-30 20:38:21 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
   
+  attr_accessor :owner
+  attr_reader :name
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = Owner.new(owner) unless Owner.all.include?(owner)
+    
+  end
+  
+  
+  
+  
 end
